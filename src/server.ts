88f694b1be15b6082263d5df4f8c75ef8bd77827
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-08-04 21:56:27 -0300

    Configure helmet

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
+import helmet from 'helmet';
 
 import routes from './routes';
 
 const app = express();
 
+app.use(helmet);
 app.use(cors());
 app.use(express.json());
 app.use('/v1', routes);
