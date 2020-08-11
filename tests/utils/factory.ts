3600533a442ba8ec77400add1cb3367c4f9e3779
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-08-11 20:46:20 -0300

    Create factories

--- tests/utils/factory.ts
+++ tests/utils/factory.ts
@@ -2,12 +2,45 @@ import factory from 'factory-girl';
 import faker from 'faker';
 
 factory.define(
+  'User',
+  {},
+  {
+    id: faker.random.number,
+    name: faker.name.findName,
+    avatar: faker.image.imageUrl,
+    whatsapp: faker.phone.phoneNumber,
+    bio: faker.lorem.paragraph,
+  },
+);
+
+factory.define(
   'Class',
   {},
   {
     id: faker.random.number,
     subject: faker.name.title,
     cost: () => Number(faker.finance.amount()),
+    user_id: faker.random.number,
+  },
+);
+
+factory.define('ClassSchedule', {}, () => {
+  const from = faker.random.number({ min: 0, max: 23 });
+  const to = faker.random.number({ min: from + 1, max: 24 });
+
+  return {
+    id: faker.random.number,
+    week_day: () => faker.random.number({ min: 0, max: 6 }),
+    from: from * 60,
+    to: to * 60,
+    class_id: faker.random.number,
+  };
+});
+
+factory.define(
+  'Connection',
+  {},
+  {
     user_id: faker.random.number,
   },
 );
