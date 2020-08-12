1da33029a0678c535866d0422714cadbc0348ce4
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-08-11 21:00:36 -0300

    Change file name to update github cache (maybe)

--- src/utils/convertStringHourToMinutes.ts
+++ src/utils/convertStringHourToMinutes.ts
@@ -1,4 +1,4 @@
-export default function convertHourToMinutes(time: string): number {
+export default function convertStringHourToMinutes(time: string): number {
   const [hour, minutes] = time.split(':').map(Number);
   return hour * 60 + minutes;
 }
