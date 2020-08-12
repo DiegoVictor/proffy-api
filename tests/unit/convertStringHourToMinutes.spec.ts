433b8f0eee22484525b70784325f7c7330d4706d
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-08-11 21:07:11 -0300

    Fix filename

---
+++ tests/unit/convertStringHourToMinutes.spec.ts
@@ -0,0 +1,8 @@
+import convertStringHourToMinutes from '../../src/utils/convertStringHourToMinutes';
+
+describe('convertStringHourToMinutes', () => {
+  it('should be able to convert hour to minutes', () => {
+    const hour = convertStringHourToMinutes('9:00');
+    expect(hour).toBe(540);
+  });
+});
