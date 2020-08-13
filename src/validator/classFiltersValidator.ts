aaeb85b347739cc411b29f5c8129d7d848a045e5
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-08-12 22:41:20 -0300

    Validate time param

--- src/validator/classFiltersValidator.ts
+++ src/validator/classFiltersValidator.ts
@@ -4,6 +4,9 @@ export default celebrate({
   [Segments.QUERY]: Joi.object().keys({
     week_day: Joi.number().min(0).max(7).required(),
     subject: Joi.string().required(),
-    time: Joi.string().required(),
+    time: Joi.string()
+      .regex(/^([0-1]?[0-9]|2[0-4]):([0-5][0-9])(:[0-5][0-9])?$/, 'HH:mm')
+      .required(),
+    page: Joi.number(),
   }),
 });
