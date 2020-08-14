6299f10dde40f2db6082424ac65bdd7461a92726
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-08-13 21:21:34 -0300

    Handle hapi/boom errors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import 'dotenv/config';
-import express from 'express';
+import 'express-async-errors';
+
+import express, { NextFunction, Response, Request } from 'express';
 import cors from 'cors';
 import { errors } from 'celebrate';
 import helmet from 'helmet';
+import { isBoom } from '@hapi/boom';
 
 import routes from './routes';
 import RouteAliases from './middlewares/RouteAliases';
@@ -17,5 +20,18 @@ app.use(RouteAliases);
 app.use('/v1', routes);
 
 app.use(errors());
+app.use((err: Error, _: Request, res: Response, next: NextFunction) => {
+  if (isBoom(err)) {
+    const { statusCode, payload } = err.output;
+
+    return res.status(statusCode).json({
+      ...payload,
+      ...err.data,
+      docs: process.env.DOCS_URL,
+    });
+  }
+
+  return next(err);
+});
 
 export default app;
