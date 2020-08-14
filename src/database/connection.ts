c8d9da443d723719777201bddbd2c2359f296c39
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-08-14 20:02:31 -0300

    Prevent to start knex without connection

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,6 +1,19 @@
 import knex from 'knex';
 import config from '../../knexfile';
 
-const db = knex(config[process.env.NODE_ENV]);
+const db = knex(
+  (() => {
+    if (process.env?.NODE_ENV) {
+      return config[process.env.NODE_ENV];
+    }
+
+    const key: string | undefined = Object.keys(config).shift();
+    if (!key) {
+      throw new Error('Knex configuration not found');
+    }
+
+    return config[key];
+  })(),
+);
 
 export default db;
