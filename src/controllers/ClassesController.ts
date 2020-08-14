e5dbe58c7f69ded6cf2ee2b9e9616c1e437300d0
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-08-14 20:04:29 -0300

    Set default page number

--- src/controllers/ClassesController.ts
+++ src/controllers/ClassesController.ts
@@ -15,6 +15,7 @@ export default class ClassesController {
   async index(request: Request, response: Response): Promise<Response> {
     const { week_day, subject, time } = request.query;
     const limit = 10;
+    const page = Number(request.query.page) || 1;
 
     const timeInMinutes = convertStringHourToMinutes(String(time));
 
