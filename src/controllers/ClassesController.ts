4343cb090b03c2b368bdff3d6ad506cc77fb5470
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-08-15 00:43:16 -0300

    Use repository to fetch data

--- src/controllers/ClassesController.ts
+++ src/controllers/ClassesController.ts
@@ -9,35 +9,32 @@ const classesRepository = new ClassesRepository();
 export default class ClassesController {
   async index(request: Request, response: Response): Promise<Response> {
     const { current_url } = request;
-    const { week_day, subject, time } = request.query;
-    const limit = 10;
     const page = Number(request.query.page) || 1;
+    const limit = 10;
 
+    const { week_day, subject, time } = request.query;
     const timeInMinutes = convertStringHourToMinutes(String(time));
 
-    const query = () =>
-      db('classes')
-        .whereExists(function () {
-          this.select('class_schedule.*')
-            .from('class_schedule')
-            .whereRaw('`class_schedule`.`class_id` = `classes`.`id`')
-            .whereRaw('`class_schedule`.`week_day` = ??', [Number(week_day)])
-            .whereRaw('`class_schedule`.`from` <= ??', [timeInMinutes])
-            .whereRaw('`class_schedule`.`to` > ??', [timeInMinutes]);
-        })
-        .where('classes.subject', '=', String(subject))
-        .join('users', 'classes.user_id', '=', 'users.id');
-
-    const classes = await query()
+    const classes = await classesRepository
+      .getQueryBySubjectInWeekDayAtTime(
+        String(subject),
+        Number(week_day),
+        timeInMinutes,
+      )
       .limit(limit)
       .offset((page - 1) * limit)
       .select(['classes.*', 'users.*']);
 
-    const [count] = await query().count();
+    const [count] = await classesRepository
+      .getQueryBySubjectInWeekDayAtTime(
+        String(subject),
+        Number(week_day),
+        timeInMinutes,
+      )
+      .count();
     response.header('X-Total-Count', count['count(*)']);
 
     const pages_total = Math.ceil(count['count(*)'] / limit);
-
     if (pages_total > 1) {
       response.links(paginationLinks(page, pages_total, current_url));
     }
