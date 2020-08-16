2b092e6ee013835590e6c444ec7a0ff8bac6cfe2
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-08-16 10:45:23 -0300

    Add email, password and surname fields to table

--- src/database/migrations/1596581550090_create_users.ts
+++ src/database/migrations/1596581550090_create_users.ts
@@ -3,7 +3,10 @@ import Knex from 'knex';
 export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable('users', table => {
     table.increments('id').primary();
+    table.string('email').unique().notNullable();
+    table.string('password').notNullable();
     table.string('name').notNullable();
+    table.string('surname').notNullable();
     table.string('avatar').notNullable();
     table.string('whatsapp').notNullable();
     table.string('bio').notNullable();
