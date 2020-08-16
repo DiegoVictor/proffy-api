2a65b66411479f647d03ee17fbe9d3a1f2d3eed1
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-08-16 10:46:30 -0300

    Update next configuration

--- knexfile.ts
+++ knexfile.ts
@@ -1,41 +1,17 @@
 import path from 'path';
 
-interface DatabaseConfiguration {
-  client: string;
+module.exports = {
+  client: 'sqlite3',
   connection: {
-    filename: string;
-  };
+    filename: path.resolve(
+      __dirname,
+      'src',
+      'database',
+      `${process.env.NODE_ENV || 'development'}.sqlite`,
+    ),
+  },
   migrations: {
-    directory: string;
-  };
-  useNullAsDefault: true;
-}
-
-interface DatabaseConfigurationGroup {
-  [key: string]: DatabaseConfiguration;
-}
-
-const configurations: DatabaseConfigurationGroup = {
-  test: {
-    client: 'sqlite3',
-    connection: {
-      filename: path.resolve(__dirname, 'src', 'database', 'tests.sqlite'),
-    },
-    migrations: {
-      directory: path.resolve(__dirname, 'src', 'database', 'migrations'),
-    },
-    useNullAsDefault: true,
+    directory: path.resolve(__dirname, 'src', 'database', 'migrations'),
   },
-  development: {
-    client: 'sqlite3',
-    connection: {
-      filename: path.resolve(__dirname, 'src', 'database', 'database.sqlite'),
-    },
-    migrations: {
-      directory: path.resolve(__dirname, 'src', 'database', 'migrations'),
-    },
-    useNullAsDefault: true,
-  },
+  useNullAsDefault: true,
 };
-
-export default configurations;
