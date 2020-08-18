85f651c983e5889749acbbb486fb06d8e257ee77
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-08-18 20:20:41 -0300

    Create email sender provider

---
+++ src/providers/MailProvider.ts
@@ -0,0 +1,40 @@
+import nodemailer, { Transporter } from 'nodemailer';
+
+import mail from '../config/mail';
+
+interface Recipient {
+  name: string;
+  email: string;
+}
+
+interface MailProps {
+  to: Recipient;
+  from?: Recipient;
+  subject: string;
+  html: string;
+}
+
+class MailProvider {
+  private client: Transporter;
+
+  constructor() {
+    const transporter = nodemailer.createTransport(
+      `smtp://${mail.auth.user}:${mail.auth.pass}@${mail.host}:${mail.port}/`,
+    );
+    this.client = transporter;
+  }
+
+  public async sendMail({ to, from, subject, html }: MailProps): Promise<void> {
+    await this.client.sendMail({
+      from: {
+        name: from?.name || 'Equipe Proffy',
+        address: from?.email || 'noreply@proffy.com.br',
+      },
+      to: { name: to.name, address: to.email },
+      subject,
+      html,
+    });
+  }
+}
+
+export default MailProvider;
