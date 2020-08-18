1a43e18746c25d199ea8fec53bc187bfccf8db78
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-08-18 20:30:57 -0300

    Update validation

--- src/validator/classValidator.ts
+++ src/validator/classValidator.ts
@@ -2,13 +2,12 @@ import { celebrate, Segments, Joi } from 'celebrate';
 
 export default celebrate({
   [Segments.BODY]: {
-    name: Joi.string().required(),
-    avatar: Joi.string().required(),
+    user_id: Joi.number().required(),
+    bio: Joi.string().required(),
     whatsapp: Joi.string().min(11).required(),
-    bio: Joi.string().min(20).required(),
     subject: Joi.string().required(),
     cost: Joi.number().required(),
-    schedule: Joi.array().items(
+    schedules: Joi.array().items(
       Joi.object().keys({
         week_day: Joi.number().min(0).max(7).required(),
         from: Joi.string().required(),
