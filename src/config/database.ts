156e0a239185754392b6bedb09e724acd481a3b9
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-08-18 20:20:23 -0300

    Update database configuration

---
+++ src/config/database.ts
@@ -0,0 +1,3 @@
+import config from '../../knexfile';
+
+export default config;
--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,19 +1,4 @@
 import knex from 'knex';
-import config from '../../knexfile';
+import database from '../config/database';
 
-const db = knex(
-  (() => {
-    if (process.env?.NODE_ENV) {
-      return config[process.env.NODE_ENV];
-    }
-
-    const key: string | undefined = Object.keys(config).shift();
-    if (!key) {
-      throw new Error('Knex configuration not found');
-    }
-
-    return config[key];
-  })(),
-);
-
-export default db;
+export default knex(database);
