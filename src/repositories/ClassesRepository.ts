cca878097d14ef94e96732642898c49e86e5b191
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-08-18 20:28:43 -0300

    Create class repository

--- src/repositories/ClassesRepository.ts
+++ src/repositories/ClassesRepository.ts
@@ -2,22 +2,39 @@ import Knex from 'knex';
 import db from '../database/connection';
 
 class ClassesRepository {
-  getQueryBySubjectInWeekDayAtTime(
-    subject: string,
-    week_day: number,
-    time: number,
+  queryBySubjectInWeekDayAtTime(
+    subject: string | null,
+    week_day: number | null,
+    time: number | null,
   ): Knex.QueryBuilder {
-    return db('classes')
-      .whereExists(function () {
+    const query = db('classes')
+      .join('users', 'classes.user_id', '=', 'users.id')
+      .join('class_schedule', 'classes.id', '=', 'class_schedule.class_id');
+
+    if (subject) {
+      query.where('classes.subject', '=', subject);
+    }
+
+    if (week_day) {
+      query.whereExists(function () {
         this.select('class_schedule.*')
           .from('class_schedule')
           .whereRaw('`class_schedule`.`class_id` = `classes`.`id`')
-          .whereRaw('`class_schedule`.`week_day` = ??', [week_day])
+          .whereRaw('`class_schedule`.`week_day` = ??', [week_day]);
+      });
+    }
+
+    if (time) {
+      query.whereExists(function () {
+        this.select('class_schedule.*')
+          .from('class_schedule')
+          .whereRaw('`class_schedule`.`class_id` = `classes`.`id`')
           .whereRaw('`class_schedule`.`from` <= ??', [time])
           .whereRaw('`class_schedule`.`to` > ??', [time]);
-      })
-      .where('classes.subject', '=', subject)
-      .join('users', 'classes.user_id', '=', 'users.id');
+      });
+    }
+
+    return query;
   }
 }
 
