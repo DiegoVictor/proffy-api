bf6e07cf85cbfe5aa802eea2fefeaad60acdfdbd
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-08-20 20:49:15 -0300

    Update classes filter validation

--- src/validator/classFiltersValidator.ts
+++ src/validator/classFiltersValidator.ts
@@ -2,11 +2,12 @@ import { celebrate, Segments, Joi } from 'celebrate';
 
 export default celebrate({
   [Segments.QUERY]: Joi.object().keys({
-    week_day: Joi.number().min(0).max(7).required(),
-    subject: Joi.string().required(),
-    time: Joi.string()
-      .regex(/^([0-1]?[0-9]|2[0-4]):([0-5][0-9])(:[0-5][0-9])?$/, 'HH:mm')
-      .required(),
+    week_day: Joi.number().min(0).max(7),
+    subject: Joi.string(),
+    time: Joi.string().regex(
+      /^([0-1]?[0-9]|2[0-4]):([0-5][0-9])(:[0-5][0-9])?$/,
+      'HH:mm',
+    ),
     page: Joi.number(),
   }),
 });
