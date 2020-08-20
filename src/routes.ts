dacb5397e032dfdbbae08457ba911dd16d89e885
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-08-20 20:46:21 -0300

    Change user password

--- src/routes.ts
+++ src/routes.ts
@@ -2,18 +2,24 @@ import express from 'express';
 
 import ClassesController from './controllers/ClassesController';
 import ConnectionsController from './controllers/ConnectionsController';
+import ForgotPasswordController from './controllers/ForgotPasswordController';
+import ResetPasswordController from './controllers/ResetPasswordController';
 import UsersController from './controllers/UsersController';
 import SessionsController from './controllers/SessionsController';
 import userIdValidator from './validator/userIdValidator';
 import classFiltersValidator from './validator/classFiltersValidator';
 import classValidator from './validator/classValidator';
+import forgotPasswordValidator from './validator/forgotPasswordValidator';
 import userValidator from './validator/userValidator';
+import resetPasswordValidator from './validator/resetPasswordValidator';
 import sessionValidator from './validator/sessionValidator';
 import Auth from './middlewares/Auth';
 import idValidator from './validator/idValidator';
 
 const classesController = new ClassesController();
 const connectionsController = new ConnectionsController();
+const forgotPasswordController = new ForgotPasswordController();
+const resetPasswordController = new ResetPasswordController();
 const sessionsController = new SessionsController();
 const usersController = new UsersController();
 const routes = express.Router();
@@ -24,6 +30,17 @@ routes.get('/connections', connectionsController.index);
 routes.post('/connections', userIdValidator, connectionsController.store);
 
 routes.post('/users', userValidator, usersController.store);
+routes.post(
+  '/users/forgot_password',
+  forgotPasswordValidator,
+  forgotPasswordController.store,
+);
+routes.post(
+  '/users/reset_password',
+  resetPasswordValidator,
+  resetPasswordController.store,
+);
+
 routes.use(Auth);
 
 routes.get('/classes', classFiltersValidator, classesController.index);
