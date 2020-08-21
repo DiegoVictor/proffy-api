30295351c77fa4ddbd6347bf39421e0b74d94d33
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-08-21 18:58:24 -0300

    Create favorites repository

---
+++ src/repositories/FavoritesRepository.ts
@@ -0,0 +1,20 @@
+import Knex from 'knex';
+import db from '../database/connection';
+
+class FavoritesRepository {
+  queryMyFavorites(id: number): Knex.QueryBuilder {
+    const query = db('favorites')
+      .join('users', 'favorites.favorited_user_id', '=', 'users.id')
+      .join('classes', 'users.id', '=', 'classes.user_id')
+      .where('favorites.user_id', id);
+
+    return query;
+  }
+
+  async countBySubjectInWeekDayAtTime(id: number): Promise<string> {
+    const [count] = await this.queryMyFavorites(id).count();
+    return count['count(*)'];
+  }
+}
+
+export default FavoritesRepository;
