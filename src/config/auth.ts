f4ed0edd56d9e33b5d5449f89a840ad5b9c492f5
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-08-22 01:02:53 -0300

    Type auth configuration

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,4 +1,9 @@
+interface Auth {
+  secret: string;
+  expirationTime: string;
+}
+
 export default {
   secret: process.env.JWT_SECRET,
   expirationTime: process.env.JWT_EXPIRATION_TIME,
-};
+} as Auth;
