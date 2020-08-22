eb1e9249d6bb19ed2f7cc227d22d552901c68e27
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-08-22 10:37:25 -0300

    Allow another fields

--- src/validator/pageValidator.ts
+++ src/validator/pageValidator.ts
@@ -1,7 +1,7 @@
 import { celebrate, Segments, Joi } from 'celebrate';
 
 export default celebrate({
-  [Segments.QUERY]: {
+  [Segments.QUERY]: Joi.object({
     page: Joi.number(),
-  },
+  }).unknown(),
 });
