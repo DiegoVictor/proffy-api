abb6aab3fd289e41c2208d33cb345c4759182dcc
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-08-24 18:36:04 -0300

    Create more tests to classes endpoints

--- tests/integration/ClassesController.spec.ts
+++ tests/integration/ClassesController.spec.ts
@@ -142,6 +142,100 @@ describe('ClassesController', () => {
     });
   });
 
+  it('should be able to get the second page of classes', async () => {
+    const week_day = 0;
+    const subject = 'Matemátia';
+    const classesCount = 15;
+
+    const user = await factory.attrs<User>('User');
+    const [user_id] = await connection('users').insert(user);
+    const authorization = `Bearer ${token(user_id)}`;
+
+    const classes = await factory.attrsMany<Class>(
+      'Class',
+      classesCount,
+      Array.from(Array(classesCount).keys()).map(() => ({
+        user_id,
+        subject,
+      })),
+    );
+
+    await connection('classes').insert(classes);
+
+    const schedules = await factory.attrsMany<ClassSchedule>(
+      'ClassSchedule',
+      classesCount,
+      Array.from(Array(classesCount).keys()).map(() => ({
+        from: 480,
+        to: 920,
+        week_day,
+      })),
+    );
+
+    await connection('class_schedule').insert(
+      schedules.map((schedule, index) => ({
+        ...schedule,
+        class_id: index + 1,
+      })),
+    );
+
+    const response = await request(app)
+      .get(
+        `/v1/classes?week_day=${week_day}&subject=${subject}&time=9:00&page=2`,
+      )
+      .set('Authorization', authorization);
+
+    const savedClasses = await connection('classes')
+      .join('users', 'classes.user_id', '=', 'users.id')
+      .where('classes.subject', '=', subject)
+      .whereExists(function () {
+        this.select('class_schedule.*')
+          .from('class_schedule')
+          .whereRaw('`class_schedule`.`class_id` = `classes`.`id`')
+          .whereRaw('`class_schedule`.`week_day` = ??', [week_day])
+          .whereRaw('`class_schedule`.`from` <= ??', [540])
+          .whereRaw('`class_schedule`.`to` > ??', [540]);
+      })
+      .limit(10)
+      .offset(10)
+      .select(
+        'classes.id',
+        'classes.subject',
+        'classes.cost',
+        'users.id as user_id',
+        'users.name',
+        'users.email',
+        'users.surname',
+        'users.avatar',
+        'users.whatsapp',
+        'users.bio',
+      );
+
+    const savedSchedules = await connection('class_schedule')
+      .whereIn(
+        'class_id',
+        savedClasses.map(classItem => classItem.id),
+      )
+      .select('week_day', 'from', 'to', 'class_id');
+
+    const classesSerialized = savedClasses.map(classItem => {
+      return {
+        ...classItem,
+        schedules: savedSchedules.filter(
+          schedule => schedule.class_id === classItem.id,
+        ),
+      };
+    });
+
+    classesSerialized.forEach(classItem => {
+      expect(response.body).toContainEqual({
+        ...classItem,
+        url: `${url}/classes/${classItem.id}`,
+        user_url: `${url}/users/${user_id}`,
+      });
+    });
+  });
+
   it('should be able to get all classes', async () => {
     const week_day = 0;
     const subject = 'Matemátia';
@@ -231,6 +325,88 @@ describe('ClassesController', () => {
     });
   });
 
+  it('should be able to get all classes without filters', async () => {
+    const week_day = 0;
+    const subject = 'Matemátia';
+    const classesCount = 5;
+
+    const user = await factory.attrs<User>('User');
+    const [user_id] = await connection('users').insert(user);
+    const authorization = `Bearer ${token(user_id)}`;
+
+    const classes = await factory.attrsMany<Class>(
+      'Class',
+      classesCount,
+      Array.from(Array(classesCount).keys()).map(() => ({
+        user_id,
+        subject,
+      })),
+    );
+
+    await connection('classes').insert(classes);
+
+    const schedules = await factory.attrsMany<ClassSchedule>(
+      'ClassSchedule',
+      classesCount,
+      Array.from(Array(classesCount).keys()).map(() => ({
+        from: 480,
+        to: 920,
+        week_day,
+      })),
+    );
+
+    await connection('class_schedule').insert(
+      schedules.map((schedule, index) => ({
+        ...schedule,
+        class_id: index + 1,
+      })),
+    );
+
+    const response = await request(app)
+      .get(`/v1/classes`)
+      .set('Authorization', authorization);
+
+    const savedClasses = await connection('classes')
+      .join('users', 'classes.user_id', '=', 'users.id')
+      .limit(10)
+      .select(
+        'classes.id',
+        'classes.subject',
+        'classes.cost',
+        'users.id as user_id',
+        'users.name',
+        'users.email',
+        'users.surname',
+        'users.avatar',
+        'users.whatsapp',
+        'users.bio',
+      );
+
+    const savedSchedules = await connection('class_schedule')
+      .whereIn(
+        'class_id',
+        savedClasses.map(classItem => classItem.id),
+      )
+      .select('week_day', 'from', 'to', 'class_id');
+
+    const classesSerialized = savedClasses.map(classItem => {
+      return {
+        ...classItem,
+        schedules: savedSchedules.filter(
+          schedule => schedule.class_id === classItem.id,
+        ),
+      };
+    });
+
+    classesSerialized.forEach(classItem => {
+      expect(response.body).toContainEqual({
+        ...classItem,
+        url: `${url}/classes/${classItem.id}`,
+        user_url: `${url}/users/${user_id}`,
+      });
+    });
+  });
+
   it('should be able to create new class', async () => {
     const subject = 'Matemática';
     const cost = faker.random.number();
@@ -251,7 +427,7 @@ describe('ClassesController', () => {
       .send({
         user_id,
         subject,
-        bio: user.bio,
+        bio: faker.lorem.paragraph(),
         whatsapp: user.whatsapp,
         cost,
         schedules,
@@ -268,6 +444,55 @@ describe('ClassesController', () => {
 
     const classSchedule = await connection('class_schedule')
       .where('class_id', classItem.id)
+      .first();
+    expect(classSchedule).toMatchObject({
+      id: expect.any(Number),
+      week_day: 0,
+      from: 360,
+      to: 720,
+    });
+  });
+
+  it('should be able to update a class', async () => {
+    const subject = faker.lorem.word();
+    const schedules = [
+      {
+        week_day: 0,
+        from: '6:00',
+        to: '12:00',
+      },
+    ];
+    const user = await factory.attrs<User>('User');
+    const [user_id] = await connection('users').insert(user);
+    const authorization = `Bearer ${token(user_id)}`;
+
+    const classItem = await factory.attrs<Class>('Class', { user_id });
+    const [class_id] = await connection('classes').insert(classItem);
+    const cost = classItem.cost + 1;
+
+    await request(app)
+      .post(`/v1/classes`)
+      .set('Authorization', authorization)
+      .send({
+        user_id,
+        subject,
+        bio: faker.lorem.paragraph(),
+        whatsapp: user.whatsapp,
+        cost,
+        schedules,
+      });
+
+    const updateClass = await connection('classes')
+      .where('id', class_id)
+      .first();
+    expect(updateClass).toMatchObject({
+      id: expect.any(Number),
+      subject,
+      cost,
+    });
+
+    const classSchedule = await connection('class_schedule')
+      .where('class_id', class_id)
       .first();
     expect(classSchedule).toMatchObject({
       id: expect.any(Number),
@@ -311,6 +536,28 @@ describe('ClassesController', () => {
     });
   });
 
+  it('should not be able to retrieve a class that not exists', async () => {
+    const user = await factory.attrs<User>('User');
+    const [user_id] = await connection('users').insert(user);
+    const authorization = `Bearer ${token(user_id)}`;
+
+    const class_id = faker.random.number();
+
+    const response = await request(app)
+      .get(`/v1/classes/${class_id}`)
+      .set('Authorization', authorization)
+      .expect(404)
+      .send();
+
+    expect(response.body).toStrictEqual({
+      statusCode: 404,
+      error: 'Not Found',
+      message: 'Class not found',
+      code: 144,
+      docs: process.env.DOCS_URL,
+    });
+  });
+
   it('should not be able to create new class', async () => {
     const subject = 'Matemática';
     const cost = faker.random.number();
@@ -349,6 +596,46 @@ describe('ClassesController', () => {
       error: 'Internal Server Error',
       message: 'An internal server error occurred',
       code: 150,
+      docs: process.env.DOCS_URL,
+    });
+  });
+
+  it('should not be able to create new class with an user that not exists', async () => {
+    const subject = 'Matemática';
+    const cost = faker.random.number();
+    const schedules = [
+      {
+        week_day: 0,
+        from: '8:00',
+        to: '13:00',
+      },
+    ];
+    const user_id = faker.random.number();
+    const authorization = `Bearer ${token(user_id)}`;
+
+    const response = await request(app)
+      .post(`/v1/classes`)
+      .set('Authorization', authorization)
+      .expect(400)
+      .send({
+        user_id,
+        subject,
+        cost,
+        schedules,
+        bio: faker.lorem.paragraph(),
+        whatsapp: faker.phone.phoneNumber(),
+      });
+
+    const [classesCount] = await connection('classes').count();
+    const [classScheduleCount] = await connection('class_schedule').count();
+
+    expect(classesCount['count(*)']).toBe(0);
+    expect(classScheduleCount['count(*)']).toBe(0);
+    expect(response.body).toStrictEqual({
+      statusCode: 400,
+      error: 'Bad Request',
+      message: 'User not found',
+      code: 141,
       docs: process.env.DOCS_URL,
     });
   });
