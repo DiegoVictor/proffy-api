5024e55fef68d32a4e12f7fc9e606946157fa5b0
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-08-24 23:03:05 -0300

    Remove duplicated verification

--- src/helpers/paginationLinks.ts
+++ src/helpers/paginationLinks.ts
@@ -12,16 +12,14 @@ export default (
   const url = `${resource_url}?page=`;
   const links: PageLinks = {};
 
-  if (pages_total > 1) {
-    if (page < pages_total) {
-      links.last = url + pages_total;
-      links.next = url + (page + 1);
-    }
+  if (page < pages_total) {
+    links.last = url + pages_total;
+    links.next = url + (page + 1);
+  }
 
-    if (page > 1) {
-      links.first = `${url}1`;
-      links.prev = url + (page - 1);
-    }
+  if (page > 1) {
+    links.first = `${url}1`;
+    links.prev = url + (page - 1);
   }
 
   return links;
