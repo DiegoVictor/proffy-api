8f0fa642f556f2fcf3c4770f21a0cceb5db214de
Diego Victor <diegovictorgonzaga@gmail.com>
2023-02-18 00:02:06 -0300

    Update import

--- tests/unit/convertStringHourToMinutes.spec.ts
+++ tests/unit/convertStringHourToMinutes.spec.ts
@@ -1,4 +1,4 @@
-import convertStringHourToMinutes from '../../src/utils/convertStringHourToMinutes';
+import { convertStringHourToMinutes } from '../../src/utils/convertStringHourToMinutes';
 
 describe('convertStringHourToMinutes', () => {
   it('should be able to convert hour to minutes', () => {
