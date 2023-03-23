2c63b97e652f1db779b83c9700104a11f44071b5
Diego Victor <diegovictorgonzaga@gmail.com>
2023-03-23 00:38:55 -0300

    Update route path

--- src/routes/classes.ts
+++ src/routes/classes.ts
@@ -3,7 +3,6 @@ import { Router } from 'express';
 import { ClassesController } from '../controllers/ClassesController';
 import classFiltersValidator from '../validators/classFiltersValidator';
 import classValidator from '../validators/classValidator';
-import idValidator from '../validators/idValidator';
 import pageValidator from '../validators/pageValidator';
 
 const classesController = new ClassesController();
@@ -11,7 +10,7 @@ const classesController = new ClassesController();
 const app = Router();
 
 app.get('/', classFiltersValidator, pageValidator, classesController.index);
-app.get('/:id', idValidator, classesController.show);
+app.get('/my-class', classesController.show);
 app.post('/', classValidator, classesController.store);
 
 export default app;
