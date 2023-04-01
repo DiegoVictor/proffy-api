432826c3fc31a4638a78793df810dc8961ce84dc
Diego Victor <diegovictorgonzaga@gmail.com>
2023-04-01 00:28:52 -0300

    Create validator to update usr profile

--- src/validators/userValidator.ts
+++ src/validators/userValidator.ts
@@ -1,5 +1,15 @@
 import { celebrate, Segments, Joi } from 'celebrate';
 
+export const updateUserValidator = celebrate({
+  [Segments.BODY]: {
+    name: Joi.string(),
+    surname: Joi.string(),
+    avatar: Joi.string(),
+    whatsapp: Joi.string().min(11),
+    bio: Joi.string().min(20),
+  },
+});
+
 export default celebrate({
   [Segments.BODY]: {
     name: Joi.string().required(),
