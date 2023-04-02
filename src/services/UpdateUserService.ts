4abae60a0ec16c80c82d6b162f3e383aab357130
Diego Victor <diegovictorgonzaga@gmail.com>
2023-04-02 11:15:32 -0300

    Create service to update user profile data

---
+++ src/services/UpdateUserService.ts
@@ -0,0 +1,29 @@
+import { notFound } from '@hapi/boom';
+
+import { db } from '../database/sql';
+
+interface Request {
+  user_id: number;
+  data: {
+    name?: string;
+    surname?: string;
+    avatar?: string;
+    whatsapp?: string;
+    bio?: string;
+  };
+}
+
+export class UpdateUserService {
+  public async execute({ user_id, data }: Request): Promise<void> {
+    const user = await db('users').where('id', user_id).first();
+
+    if (!user) {
+      throw notFound('User not found', { code: 244 });
+    }
+
+    await db('users').update({
+      ...data,
+      id: user_id,
+    });
+  }
+}
