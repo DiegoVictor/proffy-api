caaafb13018eb9f73275a0d42027ce4b6d671c48
DiegoVictor <diegovictorgonzaga@gmail.com>
2024-02-13 09:27:18 -0300

    Remove swagger endpoint

--- src/app.ts
+++ src/app.ts
@@ -6,9 +6,7 @@ import cors from 'cors';
 import { errors } from 'celebrate';
 import helmet from 'helmet';
 import { isBoom } from '@hapi/boom';
-import swagger from 'swagger-ui-express';
 
-import swaggerDocument from './swagger.json';
 import routes from './routes';
 import RouteAliases from './middlewares/RouteAliases';
 
@@ -18,7 +16,6 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-app.use('/docs', swagger.serve, swagger.setup(swaggerDocument));
 app.use(RouteAliases);
 app.use('/v1', routes);
 
