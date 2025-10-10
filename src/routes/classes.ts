099921253bb92736b57594dda4d51a7b809972b5
DiegoVictor <diegovictorgonzaga@gmail.com>
2025-10-10 16:35:13 -0300

    Remove pageValidator from classes route and simplify classFiltersValidator query structure

--- src/routes/classes.ts
+++ src/routes/classes.ts
@@ -3,13 +3,12 @@ import { Router } from 'express';
 import { ClassesController } from '../controllers/ClassesController';
 import classFiltersValidator from '../validators/classFiltersValidator';
 import classValidator from '../validators/classValidator';
-import pageValidator from '../validators/pageValidator';
 
 const classesController = new ClassesController();
 
 const app = Router();
 
-app.get('/', classFiltersValidator, pageValidator, classesController.index);
+app.get('/', classFiltersValidator, classesController.index);
 app.get('/my-class', classesController.show);
 app.post('/', classValidator, classesController.store);
 
